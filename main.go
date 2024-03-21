3d594b93d330824c900158eb98ab538a02cc3e1e
Ayesha Perera <aypdev1995@gmail.com>
2024-03-21 13:10:12 +0000

    for loops

--- main.go
+++ main.go
@@ -2,50 +2,33 @@ package main
 
 import "fmt"
 
-func main(){
-	fmt.Println("Hello!")
+func main() {
 
-	// strings ----------------------------------------------------------------
-	var nameOne string = "mario"
-	var nameTwo = "luigi"
-	var nameThree string // this has an empty string
+	x := 0
 
-	fmt.Println(nameOne, nameTwo, nameThree)
+	// in Go while alos equal to for
+	for x < 5 {
+		fmt.Println("value of x is: ", x)
+		x++
+	}
 
-	nameOne = "peach"
-	nameThree = "bowser"
+	for i := 0; i < 5; i++ {
+		fmt.Println("value of i is: ", i)
+	}
 
-	fmt.Println(nameOne, nameTwo, nameThree)
+	names := []string{"mario", "luigi", "yoshi", "peach"}
 
-	nameFour := "yoshi" 
-	// this is a short hand of initializing a variable and you can use it only in the very first time.
-	// you can't use this way, outside a function
+	for i := 0; i < len(names); i++ {
+		fmt.Println(names[i])
+	}
 
-	fmt.Println(nameFour)
+	for index, value := range names {
+		fmt.Printf("the value at index %v is %v \n", index, value)
+	}
 
-
-
-
-
-
-	// ints ----------------------------------------------------------------	
-	var ageOne int = 20
-	var ageTwo = 30
-	ageThree := 40
-
-	fmt.Println(ageOne, ageTwo, ageThree)
-
-	// bits and memory
-	var numOne int8 = 25
-	var numTwo int8 = -128
-	var numThree uint16 = 256
-
-	fmt.Println(numOne, numTwo, numThree)
-
-	var scoreOne float32 = 25.45
-	var scoreTwo float64 = 2444444444444444444444444444444445.458777777
-	scoreThree := 1.5
-
-	fmt.Println(scoreOne, scoreTwo, scoreThree)
+	for _, value := range names {
+		fmt.Printf("the value is %v \n", value)
+	}
+	// if we don't wnat to use any variable, you can simply use undersocore
 
 }
