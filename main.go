ee1f593fa686d3526c8231b484ec1b08a1329121
Ayesha Perera <aypdev1995@gmail.com>
2024-03-22 17:29:22 +0000

    using maps

--- main.go
+++ main.go
@@ -2,50 +2,33 @@ package main
 
 import "fmt"
 
-func main(){
-	fmt.Println("Hello!")
+func main() {
+	
+	menu := map[string]float64 {  // keys are strings and the values are floats
+		"soup": 4.99,
+		"pie": 7.99,
+		"salad": 6.99,
+		"toffee pudding": 3.55,
+	}
+	fmt.Println(menu)
+	fmt.Println(menu["pie"])
 
-	// strings ----------------------------------------------------------------
-	var nameOne string = "mario"
-	var nameTwo = "luigi"
-	var nameThree string // this has an empty string
+	for k, v := range menu {
+		fmt.Println(k, "-", v)
+	}
 
-	fmt.Println(nameOne, nameTwo, nameThree)
+	// ints as key type
+	phonebook := map[int]string{
+		267584967: "mario",
+		345784398: "luigi",
+		356482456: "peach",
+	}
 
-	nameOne = "peach"
-	nameThree = "bowser"
+	fmt.Println(phonebook)
+	fmt.Println(phonebook[345784398])
 
-	fmt.Println(nameOne, nameTwo, nameThree)
-
-	nameFour := "yoshi" 
-	// this is a short hand of initializing a variable and you can use it only in the very first time.
-	// you can't use this way, outside a function
-
-	fmt.Println(nameFour)
-
-
-
-
-
-
-	// ints ----------------------------------------------------------------	
-	var ageOne int = 20
-	var ageTwo = 30
-	ageThree := 40
-
-	fmt.Println(ageOne, ageTwo, ageThree)
-
-	// bits and memory
-	var numOne int8 = 25
-	var numTwo int8 = -128
-	var numThree uint16 = 256
-
-	fmt.Println(numOne, numTwo, numThree)
-
-	var scoreOne float32 = 25.45
-	var scoreTwo float64 = 2444444444444444444444444444444445.458777777
-	scoreThree := 1.5
-
-	fmt.Println(scoreOne, scoreTwo, scoreThree)
+	// update an item
+	phonebook[345784398] = "bowser"
+	fmt.Println(phonebook)
 
 }
