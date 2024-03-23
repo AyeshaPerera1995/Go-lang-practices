4f6d2bacc503f297a53db509358ff9078baed49b
Ayesha Perera <aypdev1995@gmail.com>
2024-03-23 23:25:11 +0000

    add waitGroups and defer keyword

--- main.go
+++ main.go
@@ -2,17 +2,52 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"net/http"
+	"sync"
 )
 
-func greeter(s string) {
-	for i := 0; i < 5; i++ {
-		time.Sleep(3 * time.Second)
-		fmt.Println(s)
-	}
-}
+var wg = sync.WaitGroup{}
 
 func main() {
-	go greeter("Hello")
-	greeter("World!")
+
+	websitelist := []string {
+		"https://go.dev",
+		"https://google.com",
+		"https://fb.com",
+		"https://github.com",
+	}
+
+	for _, web := range websitelist {
+		wg.Add(1)
+		go getStatusCode(web)
+	}
+
+	wg.Wait()
+	// go greeter("Hello")
+	// greeter("World!")
 }
+
+func getStatusCode(endpoint string){
+	defer wg.Done()
+	
+	res, err := http.Get(endpoint)
+	if err != nil {
+		fmt.Println("OOPS in endpoint!")
+	}
+	fmt.Printf("%v status code for %v \n", res.StatusCode, endpoint)
+
+}
+
+
+
+
+
+
+
+
+// func greeter(s string) {
+// 	for i := 0; i < 5; i++ {
+// 		time.Sleep(3 * time.Second)
+// 		fmt.Println(s)
+// 	}
+// }
